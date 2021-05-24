625c282369e3b2b72ae3956bf34731e530c16621
wfmackey <william.mackey@grattaninstitute.edu.au>
2021-05-24 19:00:20 +1000

    update staff

--- data-raw/put-Grattan-staff.R
+++ data-raw/put-Grattan-staff.R
@@ -1,4 +1,5 @@
 library(data.table)
+library(magrittr)
 Grattan_staff <-
   fread("./data-raw/Grattan_staff.tsv", fill = TRUE) %>%
   .[, email_address := paste0(gsub(" ", ".", name, fixed = TRUE), "@grattan.edu.au")]
